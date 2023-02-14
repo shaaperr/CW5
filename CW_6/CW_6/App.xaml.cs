a897afe0fc34d93c9cbc1d8a88849d653af3d8d8
Shakira Perry <shaaperr@ut.utm.edu>
2023-02-14 14:11:01 -0600

    classwork

--- CW_6/CW_6/App.xaml.cs
+++ CW_6/CW_6/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace CW5
+namespace CW_6
 {
     /// <summary>
     /// Interaction logic for App.xaml
---
+++ CW_6/CW_6/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CW_6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OleDbConnection cn;
+        public MainWindow()
+        {
+            InitializeComponent();
+            cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\CW6.accdb");
+        }
+
+        private void See_Assets_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "select* from Assets";
+
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            
+            while (read.Read())
+            {
+                data += read[0].ToString() + "\n";
+
+            }
+        }
+
+        private void tbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string query = "select* from Assets";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            if (read.Read())
+            {
+                tbox.Text = read.GetValue(0).ToString();
+
+            }
+            cn.Close();
+
+
+
+        }
+    }
+}
